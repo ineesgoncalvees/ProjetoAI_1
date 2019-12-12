4afd94abd563cdb53e828301152f72d110d916fb
Diana <Diana_noia@hotmail.com>
2019-12-12 13:20:08 +0000

    Começar Input de Agentes

---
+++ Assets/Scripts/Agents.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Agents : MonoBehaviour
+{
+    [SerializeField]
+    private InputField userInput;
+    int nAgents = Convert.ToInt32(userInput.text);
+
+    //List<Agents> agents = new List<Agents>();
+
+    //private static void InsertAgent(List<Agents> agents)
+    //{
+    //    Agents agent;
+    //}
+
+    public void GetInput(string userInput)
+    {
+        Debug.Log(nAgents);
+    }
+
+}
